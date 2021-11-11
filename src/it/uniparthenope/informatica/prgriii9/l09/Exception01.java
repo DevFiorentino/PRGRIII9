edd9164f7e95f7141e6a53d8acd63f8c345254a4
DevXino <91088269+DevFiorentino@users.noreply.github.com>
2021-11-11 14:53:42 +0100

    Example of try-catch L16.

--- src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
+++ src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
@@ -2,6 +2,14 @@ package it.uniparthenope.informatica.prgriii9.l09;
 
 public class Exception01 {
     public static void main(String[] args) {
-
+        double sum=0.0;
+        for (String arg:args){
+            try{
+                sum = sum + Double.parseDouble(arg);
+            } catch (NumberFormatException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("Sum = " + sum);
     }
 }
